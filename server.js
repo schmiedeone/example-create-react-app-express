6f9851ac74e37c15f3e6f9b2567d5159feadc658
Ainuddin Faizan <andy@schmiede.one>
2018-02-22 12:56:00 +0100

    Add get and post for list

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+});
+
+let list = [
+  {
+    id: 1,
+    text: 'Hello',
+  },
+  {
+    id: 2,
+    text: 'This is Schmiede',
+  },
+  {
+    id: 3,
+    text: 'We are in Düsseldorf',
+  },
+  {
+    id: 4,
+    text: 'Thats is Germany!',
+  },
+];
+
+let idCounter = 5;
+
+app.get('/api/list', (req, res) => {
+  res.send({ list });
+});
+
+app.post('/api/list', (req, res) => {
+  const message = req.body.message;
+  list.push({ id: idCounter++, message });
+  res.send({ list });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
